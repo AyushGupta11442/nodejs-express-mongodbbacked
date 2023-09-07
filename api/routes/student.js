6dad9e18ff991beef6d907b02455b5a74bf09c58
AyushGupta11442 <ayushgupta1442@gmail.com>
2023-09-07 12:59:04 +0530

    database successfully

--- api/routes/student.js
+++ api/routes/student.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const studentschema = require('../modals/studentmodel');
+const mongoose = require('mongoose');
 
-router.get('/' , (req , res , next) => {
+
+
+router.get('/', (req, res, next) => {
     res.status(200).json(
         {
-            message : "this is a student get req"
+            message: "this is a student get req"
         }
     )
 });
-router.post('/' , (req , res , next) => {
-    res.status(200).json(
-        {
-            message : "this is a student post req"
-        }
+router.post('/', (req, res, next) => {
+    const student = new studentschema({
+        _id: mongoose.Schema.Types.ObjectId,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.bodyphone,
+        gender: req.body.gender,
+
+    });
+
+    student.save().then((result) => {
+        console.log(result);
+        res.status(200).json({
+            message: "successfull added"
+        });
+    }).catch(err => {console.log(err);res.status(200).json({
+        eroor:err
+    })}
     )
+
+
 });
 
 
--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express");
 
 const app = express();
-const studentroute = require('./api/routes/student')
+const studentroute = require('./api/routes/student');
+const facultyroute = require('./api/routes/faculty');
 
-app.use('/student' , studentroute)
+const mongoose = require("mongoose");
+
+
+mongoose.connect('mongodb+srv://ayush:Test1234@cluster0.zth6gbh.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connection.on('error' , err=>{
+    console.log('connection fails');
+});
+mongoose.connection.on('connected' , con=>{
+    console.log('connected with database');
+});
+
+
+const bodyparse = require('body-parser');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
+
+
+
+
+
+
+
+
+app.use('/student' , studentroute);
+app.use('/faculty' , facultyroute);
 
 app.use((req , res , next )=> {
-    res.status(200).json(
+    res.status(404).json(
         {
-            message:"app is running in the local host",
+            error:"Bad request",
         }
     )
     }
