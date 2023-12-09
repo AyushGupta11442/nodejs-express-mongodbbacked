abb28898a3fcfbc555105022afb4ee0de2452319
AyushGupta11442 <ayushgupta11442@gmail.com>
2023-12-09 20:58:56 +0530

    api done for signup and signin

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-const http = require('http');
-const app = require('./app')
-const server = http.createServer (app);
-server.listen (3000, console.log('app running'));
+const express = require("express");
+
+const app = express();
+const userroute = require('./api/routes/user');
+
+const mongoose = require("mongoose");
+
+
+mongoose.connect('mongodb+srv://ayush:ayush11442@cluster0.zth6gbh.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connection.on('error' , err=>{
+    console.log('connection fails');
+});
+mongoose.connection.on('connected' , con=>{
+    console.log('connected with database');
+});
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
+
+app.use('/signup' , userroute);
+Z
+app.use((req , res , next )=> {
+    res.status(404).json(
+        {
+            error:"Bad request",
+        }
+    )
+    }
+);
+
+module.exports = app;
